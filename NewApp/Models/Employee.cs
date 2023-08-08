2363d1f0364ce0d91821fa48337f5dcfa54ac12b
minhxmht <minhvv.xmht@gmail.com>
2023-08-08 13:56:19 +0700

    them code

---
+++ NewApp/Models/Employee.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewApp.Models
+{
+    public class Employee
+    {
+        public string HoTen { get; set; }
+        public string DiaChi { get; set; }
+        public int NamSinh { get; set; }
+        public Employee()
+        {
+            HoTen = "Ho ten mac dinh";
+            DiaChi = "dia chi mac dinh";
+            NamSinh = 1989;
+        }
+        public void NhapThongTin()
+        {
+            System.Console.WriteLine("Ho Ten = ");
+            HoTen = Console.ReadLine();
+            System.Console.WriteLine("Dia Chi = ");
+            DiaChi = Console.ReadLine();
+            System.Console.WriteLine("Nam sinh = ");
+            NamSinh = Convert.ToInt32(Console.ReadLine());
+        }
+        public void HienThi()
+        {
+            Console.Write("Nhan vien " + HoTen);
+            Console.WriteLine(" - " + DiaChi);
+            Console.WriteLine(" - " + NamSinh);
+        }
+        public static int TinhTuoi(int NamSinh)
+        {
+            return DateTime.Now.Year - NamSinh;
+        }
+    }
+}
--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,7 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int a=1;
-int b=2;
-int c=3;
-Console.WriteLine("{0}+{1}={2}",a,b,c);
+﻿using System.Drawing;
+using System.Dynamic;
+using System;
+using NewApp.Models;
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// int LCB, HeSo, PhuCap;
+// string HoTen;
+// System.Console.WriteLine("Nhap ho ten: ");
+// HoTen = Console.ReadLine();
+// System.Console.WriteLine("Nhap LCB: ");
+// LCB = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Nhap HeSo: ");
+// HeSo = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Nhap PhuCap: ");
+// PhuCap = Convert.ToInt32(Console.ReadLine());
 
+// System.Console.WriteLine(" Nhan vien {0} {1}*{2}+{3} = {4} ",HoTen, LCB,HeSo,PhuCap, LCB*HeSo+PhuCap);
+
+Employee e = new Employee();
+//e.NhapThongTin();
+e.HienThi();
